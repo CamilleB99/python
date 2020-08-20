80bb6203b7edbee2fc00db0365898401ce7fff71
CamilleB99 <camillebalo@gmail.com>
2020-08-20 16:39:01 -0400

    tuples

--- tuples_sets.py
+++ tuples_sets.py
@@ -1,5 +1,23 @@
 # A Tuple is a collection which is ordered and unchangeable. Allows duplicate members.
- 
 
+# Cretae tuple
+fruits = ('Apples', 'Oranges', 'Grapes')
+#fruits2 = tuple(('Apples', 'Oranges', 'Grapes'))
+
+# Single value needs a trailing comma
+fruits2 = ('Apples',)
+
+# Get a value
+print(fruits[1])
+
+# Can't chage value
+#fruits[0] = 'Pears'
+
+# Delelte tuple
+del fruits2
+
+print(len(fruits))
+
+print(fruits, type(fruits2))
 
 # A Set is a collection which is unordered and unindexed. No duplicate members.
